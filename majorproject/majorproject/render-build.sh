96183d1a78535eceeac786577d03e39dace08893
Anush Yadav <anushyadav135265@gmail.com>
2025-03-12 10:09:16 +0530

    Delete majorproject/majorproject/render-build.sh

--- majorproject/majorproject/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Updating package lists..."
-apt-get update
-
-echo "Installing Tesseract-OCR..."
-apt-get install -y tesseract-ocr
-
-echo "Tesseract installation completed!"
